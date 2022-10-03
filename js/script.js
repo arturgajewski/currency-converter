d5f73a4cb1679fa98c1d540a40d65ddfa6297acc
Artur Gajewski <arturgaj6@wp.pl>
2022-10-03 15:44:55 +0200

    modified/add function to shorter version

--- js/script.js
+++ js/script.js
@@ -1,105 +1,108 @@
-let formElement = document.querySelector(".js-form");
-let formFinished = document.querySelector(".form__finished");
-let formCurrency = document.querySelector(".js-currencyValue");
-let elementDOLAR = document.querySelector(".js-dolar");
-let elementEURO = document.querySelector(".js-euro");
-let elementCHF = document.querySelector(".js-chf");
-let elementGBP = document.querySelector(".js-gbp");
-let elementPLN = document.querySelector(".js-pln");
-let elementCZK = document.querySelector(".js-czk");
-let resetButton = document.querySelector(".js-button");
+{
+  const formElement = document.querySelector(".js-form");
+  const formFinished = document.querySelector(".form__finished");
+  const formCurrency = document.querySelector(".js-currencyValue");
+  const elementDOLAR = document.querySelector(".js-dolar");
+  const elementEURO = document.querySelector(".js-euro");
+  const elementCHF = document.querySelector(".js-chf");
+  const elementGBP = document.querySelector(".js-gbp");
+  const elementPLN = document.querySelector(".js-pln");
+  const elementCZK = document.querySelector(".js-czk");
+  const resetButton = document.querySelector(".js-button");
 
-formElement.addEventListener("input", () => {
-  let pln = +elementPLN.value;
-  let usd = +elementDOLAR.value;
-  let euro = +elementEURO.value;
-  let gbp = +elementGBP.value;
-  let chf = +elementCHF.value;
-  let czk = +elementCZK.value;
-  let summary = formFinished;
-  let currency = formCurrency;
+  const exchangePLNtoEURO = () => {
+    pln = +elementPLN.value;
+    usd = +elementDOLAR.value;
+    euro = +elementEURO.value;
+    gbp = +elementGBP.value;
+    chf = +elementCHF.value;
+    czk = +elementCZK.value;
+    summary = formFinished;
+    currency = formCurrency;
 
-  if (elementEURO.checked === true) {
-    summary = pln / euro;
-    currency = "EURO";
-  } else if (elementDOLAR.checked === true) {
-    summary = pln / usd;
-    currency = "USD (Dolarów)";
-  } else if (elementGBP.checked === true) {
-    summary = pln / gbp;
-    currency = "GBP (Funt Brytyjski)";
-  } else if (elementCHF.checked === true) {
-    summary = pln / chf;
-    currency = "CHF (Frank Szwajcarski)";
-  } else if (elementCZK.checked === true) {
-    summary = pln / czk;
-    currency = "CZK (Korona Czeska)";
-  }
+    if (elementEURO.checked === true) {
+      summary = pln / euro;
+      currency = "EURO";
+    } else if (elementDOLAR.checked === true) {
+      summary = pln / usd;
+      currency = "USD (Dolarów)";
+    } else if (elementGBP.checked === true) {
+      summary = pln / gbp;
+      currency = "GBP (Funt Brytyjski)";
+    } else if (elementCHF.checked === true) {
+      summary = pln / chf;
+      currency = "CHF (Frank Szwajcarski)";
+    } else if (elementCZK.checked === true) {
+      summary = pln / czk;
+      currency = "CZK (Korona Czeska)";
+    }
+    const initReset = () => {
+      formFinished.innerText = "0";
+      formCurrency.innerText = "Nie wybrano";
+    };
+    resetButton.addEventListener("click", initReset);
 
-  formFinished.innerText = summary.toFixed(2);
-  formCurrency.innerText = `  ${currency}`;
-});
+    formFinished.innerText = summary.toFixed(2);
+    formCurrency.innerText = `  ${currency}`;
+  };
+  formElement.addEventListener("input", exchangePLNtoEURO);
+}
 
-let formElementpln = document.querySelector(".js-formpln");
-let formFinishedpln = document.querySelector(".js-form__finishedpln");
-let formCurrencypln = document.querySelector(".js-currencyValuepln");
-let formCurrencyplne = document.querySelector(".js-currencyValueplne");
-let elementDOLARpln = document.querySelector(".js-dolarpln");
-let elementEUROpln = document.querySelector(".js-europln");
-let elementCHFpln = document.querySelector(".js-chfpln");
-let elementGBPpln = document.querySelector(".js-gbppln");
-let elementPln = document.querySelector(".js-how");
-let elementCZKpln = document.querySelector(".js-czkpln");
-let resetButtonpln = document.querySelector(".js-buttonpln");
+{
+  const formElement = document.querySelector(".js-formPln");
+  const formFinished = document.querySelector(".js-form__finishedPln");
+  const formCurrency = document.querySelector(".js-currencyNamePln");
+  const formCurrencyName = document.querySelector(".js-currencyName");
+  const elementDOLAR = document.querySelector(".js-dolarPln");
+  const elementEURO = document.querySelector(".js-euroPln");
+  const elementCHF = document.querySelector(".js-chfPln");
+  const elementGBP = document.querySelector(".js-gbpPln");
+  const element = document.querySelector(".js-how");
+  const elementCZK = document.querySelector(".js-czkPln");
+  const resetButton = document.querySelector(".js-buttonPln");
 
-formElementpln.addEventListener("input", () => {
-  let plnpol = +elementPln.value;
-  let usdpln = +elementDOLARpln.value;
-  let europlnz = +elementEUROpln.value;
-  let gbppln = +elementGBPpln.value;
-  let chfpln = +elementCHFpln.value;
-  let czkpln = +elementCZKpln.value;
-  let summaryplnz = formFinishedpln;
-  let currencypln = formCurrencypln;
-  let currencyplne = formCurrencyplne;
+  const exchangeCurrencyToPLN = () => {
+    pln = +element.value;
+    usd = +elementDOLAR.value;
+    euro = +elementEURO.value;
+    gbp = +elementGBP.value;
+    chf = +elementCHF.value;
+    czk = +elementCZK.value;
+    summary = formFinished;
+    currency = formCurrency;
+    currencyNameFinish = formCurrencyName;
 
-  if (elementEUROpln.checked === true) {
-    summaryplnz = europlnz * plnpol;
-    currencypln = "EURO";
-    currencyplne = "EURO";
-  } else if (elementDOLARpln.checked === true) {
-    summaryplnz = usdpln * plnpol;
-    currencypln = "USD (Dolar Amerykański)";
-    currencyplne = "USD (Dolar Amerykański)";
-  } else if (elementGBPpln.checked === true) {
-    summaryplnz = gbppln * plnpol;
-    currencypln = "GBP (Funt Brytyjski)";
-    currencyplne = "GBP (Funt Brytyjski)";
-  } else if (elementCHFpln.checked === true) {
-    summaryplnz = chfpln * plnpol;
-    currencypln = "CHF (Frank Szwajcarski)";
-    currencyplne = "CHF (Frank Szwajcarski)";
-  } else if (elementCZKpln.checked === true) {
-    summaryplnz = czkpln * plnpol;
-    currencypln = "CZK (Korona Czeska)";
-    currencyplne = "CZK (Korona Czeska)";
-  }
+    if (elementEURO.checked === true) {
+      summary = euro * pln;
+      currency = "EURO";
+      currencyNameFinish = "EURO";
+    } else if (elementDOLAR.checked === true) {
+      summary = usd * pln;
+      currency = "USD (Dolar Amerykański)";
+      currencyNameFinish = "USD (Dolar Amerykański)";
+    } else if (elementGBP.checked === true) {
+      summary = gbp * pln;
+      currency = "GBP (Funt Brytyjski)";
+      currencyNameFinish = "GBP (Funt Brytyjski)";
+    } else if (elementCHF.checked === true) {
+      summary = chf * pln;
+      currency = "CHF (Frank Szwajcarski)";
+      currencyNameFinish = "CHF (Frank Szwajcarski)";
+    } else if (elementCZK.checked === true) {
+      summary = czk * pln;
+      currency = "CZK (Korona Czeska)";
+      currencyNameFinish = "CZK (Korona Czeska)";
+    }
 
-  formFinishedpln.innerText = summaryplnz.toFixed(2);
-  formCurrencypln.innerText = `  ${currencypln}`;
-  formCurrencyplne.innerText = `  ${currencyplne}`;
-});
+    formFinished.innerText = summary.toFixed(2);
+    formCurrency.innerText = `  ${currency}`;
+    formCurrencyName.innerText = ` ${currencyNameFinish}`;
 
-resetButton.addEventListener("click", () => {
-  formFinished.innerText = "N/A";
-  formCurrency.innerText = "N/A";
-  formFinishedpln.innerText = "N/A";
-  formCurrencypln.innerText = "N/A";
-  formCurrencyplne.innerText = "N/A";
-});
-
-resetButtonpln.addEventListener("click", () => {
-  formFinishedpln.innerText = "N/A";
-  formCurrencypln.innerText = "N/A";
-  formCurrencyplne.innerText = "N/A";
-});
+    const initresetButton = () => {
+      formFinished.innerText = "0";
+      formCurrencyName.innerText = "Nie wybrano";
+    };
+    resetButton.addEventListener("click", initresetButton);
+  };
+  formElement.addEventListener("input", exchangeCurrencyToPLN);
+}
